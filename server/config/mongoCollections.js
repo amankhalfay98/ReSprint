cf5c7fcaaf11c0efe4fd39643aca6496cd84036b
Siddharth Gulati <sid441.sg@gmail.com>
2022-04-26 20:38:31 -0400

    :heavy_plus_sign: Added monog configurations

---
+++ server/config/mongoCollections.js
@@ -0,0 +1,18 @@
+const dbConnection = require('./mongoConnection');
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+module.exports = {
+  stories: getCollectionFn('Project_UserStories'),
+  comments: getCollectionFn('Comments'),
+  projects: getCollectionFn('Company_Projects'),
+};
