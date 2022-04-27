3f71d181089b7d89aecd05027cb9c397faf77ebd
Siddharth Gulati <sid441.sg@gmail.com>
2022-04-27 09:21:08 -0400

    :sparkles: Added endpoint to create a new project

--- server/data/projects.js
+++ server/data/projects.js
@@ -1,4 +1,6 @@
 const { validate: uuidValidate } = require('uuid');
+const uuid = require('uuid');
+const verify = require('../middlewares/validation');
 const mongoCollections = require('../config/mongoCollections');
 
 const projectSchema = mongoCollections.projects;
@@ -16,6 +18,49 @@ const getAllProjects = async (memberId) => {
   }
 };
 
+const addProject = async (members, master, projectName, userStories, totalSprints) => {
+  if (!Array.isArray(members)) {
+    throw Error('Members is of invalid type');
+  }
+  if (!uuidValidate(master)) {
+    throw Error('Master is of invalid type');
+  }
+  if (!verify.validString(projectName)) {
+    throw Error('Project Name is of invalid type');
+  }
+  if (!Array.isArray(userStories)) {
+    throw Error('User Stories is of invalid type');
+  }
+  if (!verify.checkIsProperNumber(totalSprints)) {
+    throw Error('Total Sprints is of invalid type');
+  }
+  for (let index = 0; index < members.length; index += 1) {
+    if (!uuidValidate(members[index])) {
+      throw Error('Member is of invalid type');
+    }
+  }
+  for (let index = 0; index < userStories.length; index += 1) {
+    if (!uuidValidate(userStories[index])) {
+      throw Error('User Stories is of invalid type');
+    }
+  }
+  try {
+    const projectsCollection = await projectSchema();
+    const projects = await projectsCollection.insertOne({
+      _id: uuid.v4(),
+      members,
+      master,
+      projectName,
+      userStories,
+      totalSprints,
+    });
+    return projects.ops[0];
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
 module.exports = {
   getAllProjects,
+  addProject,
 };
--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const { validate: uuidValidate } = require('uuid');
+const verify = require('../middlewares/validation');
 const projectData = require('../data/projects');
 
 router.get('/', async (req, res) => {
@@ -14,7 +15,7 @@ router.get('/', async (req, res) => {
   if (errorParams.length > 0) {
     return res.status(422).json({
       status: 'error',
-      message: `${errorParams} is not valid`,
+      message: `${errorParams} is not valid type`,
     });
   }
   try {
@@ -51,13 +52,61 @@ router.get('/:id', async (req, res) => {
   if (errorParams.length > 0) {
     return res.status(422).json({
       status: 'error',
-      message: `${errorParams} is not valid`,
+      message: `${errorParams} is not valid type`,
     });
   }
   try {
     project = await projectData.getProjectById(id, memberId);
   } catch (error) {
     res.json({ status: 'error', message: error.message });
+  }
+  return res.status(200).json({
+    project,
+    status: 'success',
+  });
+});
+
+router.post('/', async (req, res) => {
+  const { members, master, projectName, userStories, totalSprints } = req.body;
+  let project;
+  const errorParams = [];
+  if (!Array.isArray(members)) {
+    errorParams.push('Members');
+  }
+  if (!uuidValidate(master)) {
+    errorParams.push('Master');
+  }
+  if (!verify.validString(projectName)) {
+    errorParams.push('Project Name');
+  }
+  if (!Array.isArray(userStories)) {
+    errorParams.push('User Stories');
+  }
+  if (!verify.checkIsProperNumber(totalSprints)) {
+    errorParams.push('Total Sprints');
+  }
+  for (let index = 0; index < members.length; index += 1) {
+    if (!uuidValidate(members[index])) {
+      errorParams.push(`Member`);
+      break;
+    }
+  }
+  for (let index = 0; index < userStories.length; index += 1) {
+    if (!uuidValidate(userStories[index])) {
+      errorParams.push(`User Stories`);
+      break;
+    }
+  }
+  if (errorParams.length > 0) {
+    return res.status(422).json({
+      status: 'error',
+      message: `${errorParams} is not valid type`,
+    });
+  }
+  try {
+    project = await projectData.addProject(members, master, projectName, userStories, totalSprints);
+  } catch (error) {
+    return res.json({ status: 'error', message: error.message });
   }
   return res.status(200).json({
     project,
