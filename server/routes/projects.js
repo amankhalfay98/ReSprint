67cbcfc052af74d0d8cd05487e72a96a07287483
Siddharth Gulati <sid441.sg@gmail.com>
2022-04-28 13:35:14 -0400

    :sparkles: Get Project by id

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -6,71 +6,9 @@ const verify = require('../middlewares/validation');
 const projectData = require('../data/projects');
 // const checkIfAuthenticated = require('../middlewares/auth');
 
-router.get('/', async (req, res) => {
+const paramsValidation = (params) => {
   const errorParams = [];
-  let projects;
-  const { memberId } = req.body;
-  if (memberId && !uuidValidate(memberId)) {
-    errorParams.push('memberId');
-  }
-  if (errorParams.length > 0) {
-    return res.status(422).json({
-      status: 'error',
-      message: `${errorParams} is not valid type`,
-    });
-  }
-  try {
-    projects = await projectData.getAllProjects(memberId);
-    if (projects.error) {
-      return res.status(422).json({
-        status: 'error',
-        message: projects.message,
-      });
-    }
-  } catch (error) {
-    res.status().json({ status: 'error', message: error.message });
-  }
-  return res.status(200).json({
-    projects,
-    status: 'success',
-  });
-});
-
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  let project;
-  const errorParams = [];
-  if (!uuidValidate(id)) {
-    res.status(422).json({
-      status: 'error',
-      message: 'Invalid Project Id type',
-    });
-  }
-  const { memberId } = req.body;
-  if (memberId && !uuidValidate(memberId)) {
-    errorParams.push('memberId');
-  }
-  if (errorParams.length > 0) {
-    return res.status(422).json({
-      status: 'error',
-      message: `${errorParams} is not valid type`,
-    });
-  }
-  try {
-    project = await projectData.getProjectById(id, memberId);
-  } catch (error) {
-    res.json({ status: 'error', message: error.message });
-  }
-  return res.status(200).json({
-    project,
-    status: 'success',
-  });
-});
-
-router.put('/', async (req, res) => {
-  const { id, members, master, projectName, userStories, totalSprints, company } = req.body;
-  let project;
-  const errorParams = [];
+  const { id, members, master, projectName, userStories, totalSprints, company } = params;
   if (!Array.isArray(members)) {
     errorParams.push('Members');
   }
@@ -104,6 +42,80 @@ router.put('/', async (req, res) => {
       break;
     }
   }
+  return errorParams;
+};
+
+router.get('/', async (req, res) => {
+  const errorParams = [];
+  let projects;
+  const { memberId } = req.body;
+  if (memberId && !uuidValidate(memberId)) {
+    errorParams.push('memberId');
+  }
+  if (errorParams.length > 0) {
+    return res.status(422).json({
+      status: 'error',
+      message: `${errorParams} is not valid type`,
+    });
+  }
+  try {
+    projects = await projectData.getAllProjects(memberId);
+    if (projects.error) {
+      return res.status(422).json({
+        status: 'error',
+        message: projects.message,
+      });
+    }
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return res.status(422).json({ status: 'error', message: error.message });
+    }
+    return res.status(500).json({ status: 'error', message: error.message });
+  }
+  return res.status(200).json({
+    projects,
+    status: 'success',
+  });
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { memberId } = req.body;
+  let project;
+  const errorParams = [];
+  if (!uuidValidate(id)) {
+    res.status(422).json({
+      status: 'error',
+      message: 'Invalid Project Id type',
+    });
+  }
+  if (memberId && !uuidValidate(memberId)) {
+    errorParams.push('memberId');
+  }
+  if (errorParams.length > 0) {
+    return res.status(422).json({
+      status: 'error',
+      message: `${errorParams} is not valid type`,
+    });
+  }
+  try {
+    project = await projectData.getProjectById(memberId, id);
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return res.status(422).json({ status: 'error', message: error.message });
+    }
+    return res.status(500).json({ status: 'error', message: error.message });
+  }
+  return res.status(200).json({
+    project,
+    status: 'success',
+  });
+});
+
+router.put('/', async (req, res) => {
+  const { id, members, master, projectName, userStories, totalSprints, company } = req.body;
+  let project;
+  const errorParams = paramsValidation(req.body);
   if (errorParams.length > 0) {
     return res.status(422).json({
       status: 'error',
@@ -121,7 +133,10 @@ router.put('/', async (req, res) => {
       id
     );
   } catch (error) {
-    return res.json({ status: 'error', message: error.message });
+    if (error instanceof TypeError) {
+      return res.status(422).json({ status: 'error', message: error.message });
+    }
+    return res.status(500).json({ status: 'error', message: error.message });
   }
   if (project.updatedExisting) {
     return res.status(200).json({
