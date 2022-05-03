623ed366830a5cdaa945d8cb1f71678fc764a1ae
aaditipatange <apatange@stevens.edu>
2022-05-02 20:36:06 -0400

    Initial page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './img/tvm-header-logo.png';
 import './App.css';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Backlog from './components/Backlog';
+import Kanban from './components/Kanban';
+import Retrospective from './components/Retrospective';
+import UserStories from './components/UserStories';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          {/* <img src={logo} className='App-logo' alt='logo' /> */}
+          <h1 className='App-title'>
+            ReSprint
+          </h1>
+          <Link className='showlink' to='/'>
+            Home
+          </Link>
+          <Link className='showlink' to='/projects'>
+            Projects
+          </Link>
+          <Link className='showlink' to='/kanban'>
+            Kanban
+          </Link>
+          <Link className='showlink' to='/bavklog'>
+            Backlog
+          </Link>
+          <Link className='showlink' to='/userstories'>
+            UserStories
+          </Link>
+          <Link className='showlink' to='/retrospective'>
+            Retrospective
+          </Link>
+        </header>
+        <br />
+        <br />
+        <div className='App-body'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/kanban' element={<Kanban />} />
+            <Route path='/backlog' element={<Backlog />} />
+            <Route path='/userstories' element={<UserStories />} />
+            <Route path='/retrospective' element={<Retrospective />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
