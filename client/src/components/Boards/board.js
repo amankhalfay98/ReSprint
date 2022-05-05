7e71383d76b214b70b2e545acb1158e64fbfdc7e
aaditipatange <apatange@stevens.edu>
2022-05-05 17:15:30 -0400

    :globe_with_meridians: build cards kanban

--- client/src/components/Boards/board.js
+++ client/src/components/Boards/board.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './board.css';
 import {MoreHorizontal} from 'react-feather';
 import Card from '../Card/card';
+import Editable from '../Editable/editable'
 
 function board() {
   return (
@@ -10,9 +11,12 @@ function board() {
             <p className='board_top_title'>To Do <span>4</span> </p>
             <MoreHorizontal/>
         </div>
-        <div className='board_cards'>
+        <div className='board_cards custom_scroll'>
             <Card/>
             <Card/>
+            <Editable displayClass='board_cards_add'
+            text='Add Card'
+            placeholder = 'Enter Card title'/>
         </div>
     </div>
   )
--- client/src/components/UserStories.js
+++ client/src/components/UserStories.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import '../App.css';
+//import axios from 'axios';
 
-const Projects = () => {
+// {
+//   "id": "userstory ID";
+//   "createdBy": "67e3823e-73e0-4c97-b31e-63c0fed6c9ec",
+//   "assignedTo": "67e3823e-73e0-4c97-b31e-63c0fed6c9ec",
+//   "comments": [
+//       "658e7f4b-483c-4ceb-8a49-525c25e62e80",
+//       "2592e4ab-7a0f-4868-be12-987546e72c63"
+//   ],
+//   "createdAt": "2022-04-29T04:28:11.429Z",
+//   "description": "This is an updated test description",
+//   "modifiedAt": "2022-04-29T04:28:11.429Z",
+//   "priority": 3,
+//   "sprint": 1,
+//   "status": "To do",
+//   "storyPoint": 5,
+//   "title": "This is a test title",
+//   "type": "Feature"
+// }
+const UserStories = () => {
+  
+
   return (
     <div>
-      User Stories
+      {/* <button onClick={getProducts()}>Click me</button> */}
       
     </div>
   );
 };
 
-export default Projects;
+export default UserStories;
