c292e01fe55b56ef0cd930e75c7864b69c56cfc2
Siddharth Gulati <sid441.sg@gmail.com>
2022-05-05 23:58:23 -0400

    :sparkles: Comments endpoints added

---
+++ server/data/comments.js
@@ -0,0 +1,153 @@
+const { validate: uuidValidate } = require('uuid');
+const uuid = require('uuid');
+const verify = require('../middlewares/validation');
+const mongoCollections = require('../config/mongoCollections');
+
+const commentSchema = mongoCollections.comments;
+const storiesSchema = mongoCollections.stories;
+
+const getAllComments = async (projectId, storyId) => {
+  if (projectId && !uuidValidate(projectId)) {
+    throw TypeError('Project Id is of invalid type');
+  }
+  if (storyId && !uuidValidate(storyId)) {
+    throw TypeError('Story Id is of invalid type');
+  }
+  const query = {};
+  if (projectId) {
+    query.projectId = projectId;
+  }
+  if (storyId) {
+    query.storyId = storyId;
+  }
+  try {
+    const commentsCollection = await commentSchema();
+    let comments = await commentsCollection.find(query).toArray();
+    if (comments !== null) {
+      comments = comments.map((x) => {
+        const val = x;
+        /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+        val.id = val._id;
+        /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+        delete val._id;
+        return val;
+      });
+    }
+    return comments;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+const getCommentById = async (id) => {
+  if (!uuidValidate(id)) {
+    throw TypeError('Id is of invalid type');
+  }
+  try {
+    const commentsCollection = await commentSchema();
+    const comment = await commentsCollection.findOne({ _id: id });
+    if (comment !== null) {
+      /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+      comment.id = comment._id;
+      /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+      delete comment._id;
+    }
+    return comment;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+const addComment = async (userId, name, comment, projectId, storyId) => {
+  if (!uuidValidate(userId)) {
+    throw TypeError('User Id is of invalid type');
+  }
+  if (!uuidValidate(projectId)) {
+    throw TypeError('Project Id is of invalid type');
+  }
+  if (!uuidValidate(storyId)) {
+    throw TypeError('Story Id is of invalid type');
+  }
+  if (!name || !verify.validString(name)) {
+    throw TypeError('Name is missing or is of invalid type');
+  }
+  if (!comment || !verify.validString(comment)) {
+    throw TypeError('Comment is missing or is of invalid type');
+  }
+  const commentDocument = {
+    _id: uuid.v4(),
+    name,
+    comment,
+    projectId,
+    storyId,
+  };
+  const commentsCollection = await commentSchema();
+  const addedComment = await commentsCollection.insertOne(commentDocument);
+  const newId = addedComment.insertedId;
+  if (addedComment !== null) {
+    const storiesCollection = await storiesSchema();
+    await storiesCollection.updateOne(
+      {
+        _id: storyId,
+      },
+      { $push: { comments: newId } }
+    );
+  }
+  const addedCommentData = await getCommentById(newId);
+  return addedCommentData;
+};
+
+const updateComment = async (id, comment, userId, name) => {
+  if (id && !uuidValidate(id)) {
+    throw TypeError('Id is of invalid type');
+  }
+  if (userId && !uuidValidate(userId)) {
+    throw TypeError('User Id is of invalid type');
+  }
+  if (comment && !verify.validString(comment)) {
+    throw TypeError('Comment is missing or is of invalid type');
+  }
+  if (name && !verify.validString(name)) {
+    throw TypeError('Name is missing or is of invalid type');
+  }
+  const query = {};
+  if (userId) {
+    query.userId = userId;
+  }
+  if (comment) {
+    query.comment = comment;
+  }
+  if (name) {
+    query.name = name;
+  }
+  try {
+    const commentsCollection = await commentSchema();
+    await commentsCollection.updateOne({ _id: id }, { $set: query });
+    const updatedComment = await getCommentById(id);
+    return updatedComment;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+const deleteComment = async (id) => {
+  let comment;
+  if (!uuidValidate(id)) {
+    throw TypeError('Id is of invalid type');
+  }
+  try {
+    const commentsCollection = await commentSchema();
+    comment = await commentsCollection.deleteOne({ _id: id });
+    return comment;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+module.exports = {
+  getAllComments,
+  getCommentById,
+  addComment,
+  deleteComment,
+  updateComment,
+};
---
+++ server/routes/comments.js
@@ -0,0 +1,182 @@
+const express = require('express');
+
+const router = express.Router();
+const { validate: uuidValidate } = require('uuid');
+const verify = require('../middlewares/validation');
+const commentData = require('../data/comments');
+// const checkIfAuthenticated = require('../middlewares/auth');
+
+router.get('/', async (req, res) => {
+  const errorParams = [];
+  let comments;
+  const { projectId, storyId } = req.query;
+  if (projectId && !uuidValidate(projectId)) {
+    errorParams.push('Project Id');
+  }
+  if (storyId && !uuidValidate(storyId)) {
+    errorParams.push('Story Id');
+  }
+  if (errorParams.length > 0) {
+    return res.status(422).json({
+      status: 'error',
+      message: `${errorParams} is of invalid type`,
+    });
+  }
+  try {
+    comments = await commentData.getAllComments(projectId, storyId);
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return res.status(422).json({ status: 'error', message: error.message });
+    }
+    return res.status(500).json({ status: 'error', message: error.message });
+  }
+  return res.status(200).json({
+    comments,
+    status: 'success',
+  });
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  let comment;
+  const errorParams = [];
+  if (!uuidValidate(id)) {
+    errorParams.push('Id');
+  }
+  if (errorParams.length > 0) {
+    return res.status(422).json({
+      status: 'error',
+      message: `${errorParams} is of invalid type`,
+    });
+  }
+  try {
+    comment = await commentData.getCommentById(id);
+    if (comment === null)
+      return res.status(404).json({
+        status: 'error',
+        message: 'Comment Not Found',
+      });
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return res.status(422).json({ status: 'error', message: error.message });
+    }
+    return res.status(500).json({ status: 'error', message: error.message });
+  }
+  return res.status(200).json({
+    comment,
+    status: 'success',
+  });
+});
+
+router.post('/', async (req, res) => {
+  const { userId, name, comment, projectId, storyId } = req.body;
+  let newComment;
+  const errorParams = [];
+  if (!verify.validString(name)) {
+    errorParams.push('Name');
+  }
+  if (!verify.validString(comment)) {
+    errorParams.push('Comment');
+  }
+  if (!uuidValidate(projectId)) {
+    errorParams.push('Project Id');
+  }
+  if (!uuidValidate(storyId)) {
+    errorParams.push('Story Id');
+  }
+  if (errorParams.length > 0) {
+    return res.status(422).json({
+      status: 'error',
+      message: `${errorParams} is of invalid type`,
+    });
+  }
+  try {
+    newComment = await commentData.addComment(userId, name, comment, projectId, storyId);
+    if (newComment === null)
+      return res.status(404).json({
+        status: 'error',
+        message: 'Comment Not Found',
+      });
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return res.status(422).json({ status: 'error', message: error.message });
+    }
+    return res.status(500).json({ status: 'error', message: error.message });
+  }
+  return res.status(200).json({
+    comment: newComment,
+    status: 'success',
+  });
+});
+
+router.patch('/', async (req, res) => {
+  const { id, comment, userId, name } = req.body;
+  let updatedcomment;
+  const errorParams = [];
+  if (id && !uuidValidate(id)) {
+    errorParams.push('Id');
+  }
+  if (userId && !uuidValidate(userId)) {
+    errorParams.push('User Id');
+  }
+  if (comment && !verify.validString(comment)) {
+    errorParams.push('Comment');
+  }
+  if (name && !verify.validString(name)) {
+    errorParams.push('Name');
+  }
+  if (errorParams.length > 0) {
+    return res.status(422).json({
+      status: 'error',
+      message: `${errorParams} is of invalid type`,
+    });
+  }
+  try {
+    updatedcomment = await commentData.updateComment(id, comment, userId, name);
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return res.status(422).json({ status: 'error', message: error.message });
+    }
+    return res.status(500).json({ status: 'error', message: error.message });
+  }
+  return res.status(200).json({
+    comment: updatedcomment,
+    status: 'success',
+  });
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const errorParams = [];
+  if (!uuidValidate(id)) {
+    res.status(422).json({
+      status: 'error',
+      message: 'Project Id is of invalid type',
+    });
+  }
+  if (errorParams.length > 0) {
+    return res.status(422).json({
+      status: 'error',
+      message: `${errorParams} is of invalid type`,
+    });
+  }
+  try {
+    const comment = await commentData.getCommentById(id);
+    if (comment === null)
+      return res.status(404).json({
+        status: 'error',
+        message: 'Project Not Found',
+      });
+    await commentData.deleteComment(id);
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return res.status(422).json({ status: 'error', message: error.message });
+    }
+    return res.status(500).json({ status: 'error', message: error.message });
+  }
+  return res.status(200).json({
+    status: 'success',
+  });
+});
+
+module.exports = router;
