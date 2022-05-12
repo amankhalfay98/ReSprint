573908fd1103e0343ade423badb02562d39c8295
Siddharth Gulati <sid441.sg@gmail.com>
2022-05-12 03:28:48 -0400

    :sparkles: Update a user

--- server/routes/users.js
+++ server/routes/users.js
@@ -111,4 +111,69 @@ router.get('/', async (req, res) => {
   });
 });
 
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { email, isScrumMaster, userName, projects, company } = req.body;
+  let user = null;
+  const errorParams = [];
+  if (!uuidValidate(id)) {
+    errorParams.push('Id');
+  }
+  if (!verify.validEmail(email)) {
+    errorParams.push('Email');
+  }
+  if (!verify.validBoolean(isScrumMaster)) {
+    errorParams.push('isScrumMaster');
+  }
+  if (!verify.validString(userName)) {
+    errorParams.push('Username');
+  }
+  if (!Array.isArray(projects)) {
+    errorParams.push('Projects');
+  }
+  if (!uuidValidate(company)) {
+    errorParams.push('Company');
+  }
+  if (projects.length > 0) {
+    for (let index = 0; index < projects.length; index += 1) {
+      if (!uuidValidate(projects[index])) {
+        errorParams.push(`Project Id ${projects[index]}`);
+      }
+      // eslint-disable-next-line
+      if ((await projectsData.getProjectById(projects[index])) === null) {
+        return res.status(404).json({
+          status: 'error',
+          message: 'Project Not Found',
+        });
+      }
+    }
+    if ((await companyData.getCompanyById(company)) === null) {
+      return res.status(404).json({ status: 'error', message: 'Company Not Found' });
+    }
+  }
+  if (errorParams.length > 0) {
+    return res.status(422).json({
+      status: 'error',
+      message: `${errorParams} is of invalid type`,
+    });
+  }
+  try {
+    user = await userData.updateUser(id, email, isScrumMaster, userName, projects, company);
+    if (user === null)
+      return res.status(404).json({
+        status: 'error',
+        message: 'User Not Found',
+      });
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return res.status(422).json({ status: 'error', message: error.message });
+    }
+    return res.status(500).json({ status: 'error', message: error.message });
+  }
+  return res.status(200).json({
+    user,
+    status: 'success',
+  });
+});
+
 module.exports = router;
