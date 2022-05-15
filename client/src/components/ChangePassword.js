8703c93ea0f013c1d9cf4928559f7c8de8944f15
Siddharth Gulati <sid441.sg@gmail.com>
2022-05-15 18:41:09 -0400

    :sparkles: Update Password

--- client/src/components/ChangePassword.js
+++ client/src/components/ChangePassword.js
@@ -1,96 +1,109 @@
-import React, { useContext, useState } from 'react';
-import { AuthContext } from '../firebase/Auth';
-import { doChangePassword } from '../firebase/FirebaseFunctions';
-import '../App.css';
+import React, { useContext, useState, useRef } from "react";
+import { AuthContext } from "../firebase/Auth";
+import { doChangePassword } from "../firebase/FirebaseFunctions";
+import { Form, Button, Card, Container } from "react-bootstrap";
+import "../App.css";
 
 function ChangePassword() {
-	const { currentUser } = useContext(AuthContext);
-	const [pwMatch, setPwMatch] = useState('');
-	console.log(currentUser);
+  const { currentUser } = useContext(AuthContext);
+  const [pwMatch, setPwMatch] = useState("");
+  console.log(currentUser);
+  const [loading, setLoading] = useState(false);
+  const currentPasswordRef = useRef();
+  const newPasswordOneRef = useRef();
+  const newPasswordTwoRef = useRef();
 
-	const submitForm = async (event) => {
-		event.preventDefault();
-		const { currentPassword, newPasswordOne, newPasswordTwo } =
-			event.target.elements;
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (newPasswordOneRef.current.value !== newPasswordTwoRef.current.value) {
+      setPwMatch("New Passwords do not match, please try again");
+      return false;
+    }
 
-		if (newPasswordOne.value !== newPasswordTwo.value) {
-			setPwMatch('New Passwords do not match, please try again');
-			return false;
-		}
-
-		try {
-			await doChangePassword(
-				currentUser.email,
-				currentPassword.value,
-				newPasswordOne.value
-			);
-			alert('Password has been changed, you will now be logged out');
-		} catch (error) {
-			alert(error);
-		}
-	};
-	if (currentUser.providerData[0].providerId === 'password') {
-		return (
-			<div>
-				{pwMatch && <h4 className="error">{pwMatch}</h4>}
-				<h2>Change Password</h2>
-				<form onSubmit={submitForm}>
-					<div className="form-group">
-						<label>
-							Current Password:
-							<input
-								className="form-control"
-								name="currentPassword"
-								id="currentPassword"
-								type="password"
-								placeholder="Current Password"
-								required
-							/>
-						</label>
-					</div>
-
-					<div className="form-group">
-						<label>
-							New Password:
-							<input
-								className="form-control"
-								name="newPasswordOne"
-								id="newPasswordOne"
-								type="password"
-								placeholder="Password"
-								required
-							/>
-						</label>
-					</div>
-					<div className="form-group">
-						<label>
-							Confirm New Password:
-							<input
-								className="form-control"
-								name="newPasswordTwo"
-								id="newPasswordTwo"
-								type="password"
-								placeholder="Confirm Password"
-								required
-							/>
-						</label>
-					</div>
-
-					<button type="submit">Change Password</button>
-				</form>
-				<br />
-			</div>
-		);
-	} else {
-		return (
-			<div>
-				<h2>
-					You are signed in using a Social Media Provider, You cannot change
-					your password
-				</h2>
-			</div>
-		);
-	}
+    try {
+      setLoading(true);
+      await doChangePassword(
+        currentUser.email,
+        currentPasswordRef.current.value,
+        newPasswordTwoRef.current.value
+      );
+      alert("Password has been changed, you will now be logged out");
+    } catch (error) {
+      alert(error);
+    }
+    setLoading(false);
+    window.location.href = "/signin";
+  }
+  if (currentUser.providerData[0].providerId === "password") {
+    return (
+      <>
+        <Container
+          className="d-flex align-items-center justify-content-center"
+          style={{ maxHeight: "h-100" }}
+        >
+          <div className="w-100" style={{ maxWidth: "600px" }}>
+            <Card>
+              <Card.Body>
+                <h1 className="text-center mb-4">{`Hi  ${currentUser.displayName}!`}</h1>
+                {pwMatch && <h2 className="error">{pwMatch}</h2>}
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group id="currentPassword">
+                    <Form.Label
+                      htmlFor="currentPassword"
+                      className="label-form"
+                    >
+                      Current Password
+                    </Form.Label>
+                    <Form.Control
+                      id="currentPassword"
+                      type="password"
+                      ref={currentPasswordRef}
+                      required
+                    ></Form.Control>
+                  </Form.Group>
+                  <Form.Group id="newPasswordOne">
+                    <Form.Label htmlFor="newPasswordOne" className="label-form">
+                      New Password
+                    </Form.Label>
+                    <Form.Control
+                      id="newPasswordOne"
+                      type="password"
+                      ref={newPasswordOneRef}
+                      required
+                    ></Form.Control>
+                  </Form.Group>
+                  <Form.Group id="newPasswordTwo">
+                    <Form.Label htmlFor="newPasswordTwo" className="label-form">
+                      Confirm New Password
+                    </Form.Label>
+                    <Form.Control
+                      id="newPasswordTwo"
+                      type="password"
+                      ref={newPasswordTwoRef}
+                      required
+                    ></Form.Control>
+                  </Form.Group>
+                  <br />
+                  <Button disabled={loading} className="w-100" type="submit">
+                    Submit
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </div>
+        </Container>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <h1>
+          You are signed in using a Social Media Provider, You cannot change
+          your password
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default ChangePassword;
