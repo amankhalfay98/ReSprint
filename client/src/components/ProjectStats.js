998f00ad6b3bb8d1efe468d6cbd103741dc6ba7d
amankhalfay98 <48147249+amankhalfay98@users.noreply.github.com>
2022-05-14 23:44:42 -0400

    added statistics page

---
+++ client/src/components/ProjectStats.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../firebase/Auth';
+import { NavLink } from 'react-router-dom';
+import '../App.css';
+import Api from '../services/api';
+
+const Projects = () => {
+	const { currentUser } = useContext(AuthContext);
+	let card = null;
+	const [projectData, setProjectData] = useState(undefined);
+	const [user, setUser] = useState(undefined);
+	const [company, setCompany] = useState(undefined);
+	let initialarray = [];
+	const [storyData, setStoryData] = useState(initialarray);
+
+	const api = new Api();
+	useEffect(() => {
+		async function getUserById() {
+			try {
+				const { user } = await api.getUserById(currentUser.uid);
+				//console.log(user);
+				if (user) {
+					setUser(user);
+					try {
+						const { company } = await api.getCompanyById(user.company);
+						setCompany(company);
+						const { projects } = await api.getAllProjects(user.company);
+						//console.log(projects);
+						if (projects) setProjectData(projects);
+						for (let i = 0; i < projectData.length; i++) {
+							const { stories } = await api.getStories(projectData[i].id);
+							console.log('story', stories);
+							if (stories)
+								setStoryData((initialarray) => [...initialarray, stories]);
+
+							console.log('storyData:', storyData);
+						}
+					} catch (error) {
+						console.log(error.message);
+					}
+				}
+			} catch (error) {
+				console.log(error.message);
+			}
+		}
+		getUserById();
+	}, [currentUser]);
+
+	// const buildCard = (project) => {
+	// 	storyData.map((story) => {
+	// 		console.log('story', story);
+	// 		if (story.projectId == project.id)
+	// 			return (
+	// 				<div className="project_card" key={project.id}>
+	// 					<p>Project Name:{project.projectName}</p>
+	// 					<li>Total Sprints:{project.totalSprints}</li>
+	// 					<p>Story title: {story.title}</p>
+	// 				</div>
+	// 			);
+	// 	});
+	// };
+
+	async function getstory(projectid) {
+		const stories = await api.getStories(projectid);
+		console.log('stories', stories);
+	}
+
+	const buildCard = (project) => {
+		return (
+			<div className="project_card" key={project.id}>
+				<p>Project Name:{project.projectName}</p>
+				<li>Total Sprints:{project.totalSprints}</li>
+			</div>
+		);
+	};
+
+	if (user && projectData && Array.isArray(projectData)) {
+		card = projectData.map((project) => {
+			getstory(project.id);
+			return buildCard(project);
+		});
+	}
+
+	return (
+		<div>
+			<ul>{card}</ul>
+		</div>
+	);
+};
+
+export default Projects;
--- client/src/components/Retrospective.js
+++ client/src/components/Retrospective.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../App.css';
+import { v4 as uuidv4 } from 'uuid';
+function Retrospective() {
+	const [state, setState] = useState({ message: '' });
 
-const Retrospective = () => {
-  return (
-    <div>
-     Retrospective
-      
-    </div>
-  );
-};
+	const listReducer = (state, action) => {
+		switch (action.type) {
+			case 'ADD_ITEM':
+				return state.concat({ name: action.name, id: action.id });
+			default:
+				throw new Error();
+		}
+	};
+
+	const initialList = [
+		{
+			id: 'a',
+			name: 'Robin',
+		},
+		{
+			id: 'b',
+			name: 'Dennis',
+		},
+	];
+
+	const [list, dispatchList] = React.useReducer(listReducer, initialList);
+	const [name, setName] = React.useState('');
+
+	function handleChange(event) {
+		// track input field's state
+		setName(event.target.value);
+	}
+
+	function handleAdd() {
+		// add item
+		dispatchList({ type: 'ADD_ITEM', name, id: uuidv4() });
+
+		setName('');
+	}
+
+	return (
+		<div>
+			<AddItem name={name} onChange={handleChange} onAdd={handleAdd} />
+
+			<List list={list} />
+		</div>
+	);
+}
+const AddItem = ({ name, onChange, onAdd }) => (
+	<div>
+		<input type="text" value={name} onChange={onChange} />
+		<button type="button" onClick={onAdd}>
+			Add
+		</button>
+	</div>
+);
+
+const List = ({ list }) => (
+	<ol>
+		{list.map((item) => (
+			<li key={item.id}>{item.name}</li>
+		))}
+	</ol>
+);
 
 export default Retrospective;
